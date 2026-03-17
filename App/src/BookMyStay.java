0f013e2d95bcec38f2ad68266441e1900ee1c65b
prithvi-s1 <prithcisoans006@gmail.com>
2026-03-17 09:02:11 +0530

    Prithvi added UseCase 1

---
+++ App/src/BookMyStay.java
@@ -0,0 +1,11 @@
+//@author prithvi Soans
+//@version 1.0
+
+public class BookMyStay
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Version: 1.0");
+    }
+}
